708230307cef88fb14ec757c68618f213a59f3cb
Vikaspat1407 <89744410+Vikaspat1407@users.noreply.github.com>
2023-09-14 12:49:24 +0000

    comit config

---
+++ .devcontainer/01_basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let score=false
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber=Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber);
+//"33"=>33
+//"33abc"=>NaN
+//"true"=>1; false=>0
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let boolValue=0
+booleanIsLoggedIn= Boolean(boolValue)
+console.log(booleanIsLoggedIn);
+//1=>true; 0=>false
+//"vikas" =>true: ""=>false
+let someNumber 
+let  stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all code as newer version
+//alert ("hello")//we are using node js ,not browser
+let name="vikas"
+let age=18
+let isLoggedIn=false
+let city
+console.log("vikas");
+//number=> 2 to power to 53
+//bigint
+//string =>""
+//boolean=>""
+//null=> standalone value
+//undefined=>
+
+//object
+console.log(typeof "vikas");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof city)
